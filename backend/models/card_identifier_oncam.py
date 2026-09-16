8ef79518300c06a9f574d0ed3d09000cec47df5a
Bejochan <okaganteng894@gmail.com>
2026-09-16 23:02:19 +0700

    Models: Mengganti menggunakan kamera hp atau settingan (1)

--- backend/models/card_identifier_oncam.py
+++ backend/models/card_identifier_oncam.py
@@ -68,7 +68,7 @@ def main():
         return
 
     # Inisialisasi Kamera dengan preferensi resolusi HD (1280x720)
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
